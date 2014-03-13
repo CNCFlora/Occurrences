8c56a63ce8c76c565c301ae39e6014e8be22ec80
Diogo Souza <diogo@diogok.net>
2014-03-13 16:41:21 -0300

    fixing vagrant bootstrap for rbenv

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,12 +12,12 @@ gem install bundler
 
 # add rbenv
 su vagrant -c 'git clone https://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv'
-su vagrant -c 'echo export PATH="/home/vagrant/.rbenv/bin:\$PATH" >> /home/vagrant/.bashrc'
-su vagrant -c 'echo eval \"\$(rbenv init -)\" >> /home/vagrant/.bashrc'
+su vagrant -c 'echo export PATH="/home/vagrant/.rbenv/bin:\$PATH" >> /home/vagrant/.profile'
+su vagrant -c 'echo eval \"\$\(rbenv init -\)\" >> /home/vagrant/.profile'
 su vagrant -c 'git clone https://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build'
 
 # initial config of app
-su vagrant -c 'cd /vagrant && rbenv install $(cat .ruby-version) && rbenv rehash'
-su vagrant -c 'cd /vagrant && bundle install'
-su vagrant -c 'cd /vagrant && [[ ! -e config.yml ]] && cp config.yml.dist config.yml'
+su vagrant -lc 'cd /vagrant && rbenv install $(cat .ruby-version) && rbenv rehash'
+su vagrant -lc 'cd /vagrant && bundle install'
+su vagrant -lc 'cd /vagrant && [[ ! -e config.yml ]] && cp config.yml.dist config.yml'
 

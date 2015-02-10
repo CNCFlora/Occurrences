c404548ca4f566d2ae72313d14c06375e1f60a48
Diogo Souza <diogo@diogok.net>
2015-02-10 14:52:13 -0200

    small changes to validation validation

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -29,44 +29,20 @@ $(function(){
 
     $(".validation-form").each(function(i,e){
         var form = $(e);
-        var taxonomy = $("input[name=taxonomy]",form);
-        var georeference = $("input[name=georeference]",form);
-        var comments = $("textarea[name=comment]",form);
         form.submit(function(){
-            var taxonomy_sel = $("input[name=taxonomy]:checked",form);
-            var georeference_sel = $("input[name=georeference]:checked",form);
-            var presence_sel = $("input[name=presence]:checked",form);
-            var duplicated_sel = $("input[name=duplicated]:checked",form);
-            var georeference_sel = $("input[name=georeference]:checked",form);
-            if(taxonomy_sel.val()=="invalid" || georeference_sel.val() == "invalid" ||
-                presence_sel.val() == 'absent' || duplicated_sel.val() == 'yes') {
-                if(comments.val().length <= 3) {
-                    alert("Necessária justificativa.");
-                    return false;
-                } else {
-                    return true;
-                }
-            }else{
-                 return true;
+            var len = $("input:checked",form).length;
+            if(len != 6) {
+              alert("É preciso responder todas as perguntas");
+              return false;
+            } else {
+              return true;
             }
         });
-        $(".form-group",form).hide();
-        taxonomy.parent().parent().show();
-        comments.parent().show();
-        function changedv()  {
-            var taxonomy_sel = $("input[name=taxonomy]:checked",form);
-            if(taxonomy_sel.val() == "invalid") {
-                $(".form-group",form).hide();
-                taxonomy.parent().parent().show();
-                comments.parent().show();
-            }  else {
-                $(".form-group",form).show();
-            }
-        };
-        taxonomy.change(function(){
-            changedv();
-        });
-        changedv();
+
+        var dup = $("input[name=duplicated]:checked",form);
+        if(dup.length ==0){
+          $("input[name=duplicated][value=no]",form).prop("checked",true);
+        }
     });
 
 });

01407bce645aeb3241b7cdccd59b62296cdb5105
Diogo Souza <diogo@diogok.net>
2015-09-21 16:19:30 -0300

    most of crud working

--- src/cncflora/repository/Occurrences.php
+++ src/cncflora/repository/Occurrences.php
@@ -47,25 +47,117 @@ class Occurrences {
 
     $result = $this->elasticsearch->search($params);
     foreach($result['hits']['hits'] as $hit) {
-      $occs[]=$this->fix($hit['_source']);
+      if(isset($hit['_source']['deleted'])) continue;
+      $occs[]=$hit['_source'];
     }
+    $occs=$this->fixAll($occs);
 
     return $occs;
   }
 
   public function getOccurrence($id) {
+    return $this->fix($this->couchdb->findDocument($id)->body);
   }
 
   public function insertOccurrence($occurrence){
+    $occurrence=$this->fix($occurrence);
+
+    $occurrence['metadata']['created']=time();
+    $occurrence['metadata']['modified']=time();
+
+    try {
+      $r=$this->couchdb->postDocument($occurrence);
+      $occurrence['_rev']=$r[1];
+      $ri=$this->elasticsearch->index([
+        'index'=>$this->db,
+        'type'=>'occurrence',
+        'id'=>$occurrence['_id'],
+        'body'=>$occurrence
+      ]);
+      sleep(1);
+      return $occurrence;
+    } catch(Exception $e) {
+      return false;
+    }
   }
 
   public function insertOccurrences($occurrences) {
+    $occurrences=$this->fixAll($occurrences);
+
+    $occurrence['metadata']['created']=time();
+    $occurrence['metadata']['modified']=time();
+
+    $bulk=$this->couchdb->createBulkUpdater();
+    $bulk->updateDocuments($occurrences);
+    $res=$bulk->execute();
+    if(isset($r->body->error)){
+      return false;
+    }
+
+    foreach($res->body as $i=>$r) {
+      $occurrences[$i]['_rev']=$r['rev'];
+      $ri=$this->elasticsearch->index([
+        'index'=>$this->db,
+        'type'=>'occurrence',
+        'id'=>$occurrences[$i]['_id'],
+        'body'=>$occurrences[$i]
+      ]);
+    }
+    sleep(1);
+
+    return $occurrences;
   }
 
   public function updateOccurrence($occurrence) {
+    $occurrence=$this->fix($occurrence);
+    $occurrence['metadata']['modified']=time();
+    try {
+      $r=$this->couchdb->postDocument($occurrence);
+      $occurrence['_rev']=$r[1];
+      $ri=$this->elasticsearch->index([
+        'index'=>$this->db,
+        'type'=>'occurrence',
+        'id'=>$occurrence['_id'],
+        'body'=>$occurrence
+      ]);
+      sleep(1);
+      return $occurrence;
+    } catch(Exception $e) {
+      return false;
+    }
   }
 
   public function updateOccurrences($occurrences) {
+    $occurrences=$this->fixAll($occurrences);
+
+    foreach($occurrences as $i=>$occ) {
+      $occurrences[$i]['metadata']['modified']=time();
+    }
+
+    $bulk=$this->couchdb->createBulkUpdater();
+    $bulk->updateDocuments($occurrences);
+    $res=$bulk->execute();
+    if(isset($r->body->error)){
+      return false;
+    }
+
+    foreach($res->body as $i=>$r) {
+      $occurrences[$i]['_rev']=$r['rev'];
+      $ri=$this->elasticsearch->index([
+        'index'=>$this->db,
+        'type'=>'occurrence',
+        'id'=>$occurrences[$i]['_id'],
+        'body'=>$occurrences[$i]
+      ]);
+    }
+    sleep(1);
+
+    return $occurrences;
+  }
+
+  public function deleteOccurrence($occurrence){
+    $occurrence['deleted']=true;
+    $this->updateOccurrence($occurrence);
   }
 
   public function canUse($occ) {
@@ -80,7 +172,47 @@ class Occurrences {
     return isset($doc["georeferenceVerificationStatus"]) && $doc["georeferenceVerificationStatus"] == "ok";
   }
 
-  public function fix($doc) {
+  public function fixAll($docs) {
+    $client = new \GuzzleHttp\Client();
+    $res = $client->request('POST',DWC_SERVICES.'/api/v1/fix',['json'=>$docs]);
+    $redocs = json_decode($res->getBody(),true);
+
+    foreach($redocs as $i=>$redoc){
+      foreach($redoc as $k=>$v) {
+        $docs[$i][$k] = $v;
+      }
+    }
+
+    foreach($docs as $i=>$doc) {
+      $docs[$i] = $this->fix($doc,false);
+    }
+
+    return $docs;
+  }
+
+  public function fix($doc,$dwc=true) {
+    if($dwc) {
+      $client = new \GuzzleHttp\Client();
+      $res = $client->request('POST',DWC_SERVICES.'/api/v1/fix',[
+        'json'=>[$doc]]);
+      $redoc = json_decode($res->getBody(),true);
+      foreach($redoc[0] as $k=>$v) {
+        $doc[$k] = $v;
+      }
+    }
+
+    if(isset($doc["occurrenceID"])) {
+      $doc["_id"] = $doc["occurrenceID"];
+    }else{
+      $doc['_id'] = 'occurrence:'.uniqid(true);
+    }
+
+    if(!isset($doc['metadata'])) {
+      $doc['metadata']=[];
+    }
+
+    $doc['metadata']['type']='occurrence';
+
     if(isset($doc["georeferenceVerificationStatus"])) {
       if($doc["georeferenceVerificationStatus"] == "1" || $doc["georeferenceVerificationStatus"] == "ok") {
         $doc["georeferenceVerificationStatus"] = "ok";
@@ -151,6 +283,7 @@ class Occurrences {
     } else {
       $doc["valid"] = "";
     }
+
     return $doc;
   }
 }
--- tests/cncflora/repository/OccurrencesTest.php
+++ tests/cncflora/repository/OccurrencesTest.php
@@ -24,6 +24,59 @@ class OccurrencesTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testCRUD() {
-      
+      $repo = new Occurrences('test0');
+
+      $newOcc = ['metadata'=>['type'=>'occurrence'],'scientificName'=>'Vicia faba'];
+      $occ=$repo->insertOccurrence($newOcc);
+
+      $this->assertNotNull($occ['metadata']['modified']);
+      $this->assertEquals($occ['metadata']['modified'],$occ['metadata']['created']);
+
+      $newOcc = ['metadata'=>['type'=>'occurrence'],'scientificName'=>'Vicia faba','institutionCode'=>'JBRJ','collectionCode'=>'RB','catalogNumber'=>'123'];
+      $occ=$repo->insertOccurrence($newOcc);
+      $this->assertEquals('urn:occurrence:JBRJ:RB:123',$occ['_id']);
+
+      $occ['decimalLatitude']='10.10';
+      $repo->updateoccurrence($occ);
+
+      $occ = $repo->getOccurrence('urn:occurrence:JBRJ:RB:123');
+      $this->assertEquals('10.10',$occ["decimalLatitude"]);
+
+      $occs = $repo->listOccurrences('Vicia faba');
+      $this->assertEquals(4,count($occs));
+      $repo->deleteOccurrence($occ);
+      $occ=$repo->getOccurrence('urn:occurrence:JBRJ:RB:123');
+      $this->assertTrue($occ["deleted"]);
+      $occs = $repo->listOccurrences('Vicia faba');
+      $this->assertEquals(3,count($occs));
+    }
+
+    public function testBulk() {
+      $repo = new Occurrences('test0');
+
+      $occs =[
+         ['scientificName'=>'vicia faba','occurrenceID'=>'123']
+        ,['scientificName'=>'vicia alba','occurrenceID'=>'456']
+      ];
+
+      $repo->insertOccurrences($occs);
+
+      $occ1=$repo->GetOccurrence('123');
+      $this->assertEquals('vicia faba',$occ1["scientificName"]);
+      $occ2=$repo->GetOccurrence('456');
+      $this->assertEquals('vicia alba',$occ2["scientificName"]);
+
+      $occ1['collector']='me';
+      $occ2['collector']='you';
+
+      $repo->updateOccurrences([$occ1,$occ2]);
+
+      $occ1=$repo->GetOccurrence('123');
+      $this->assertEquals('me',$occ1["collector"]);
+      $occ2=$repo->GetOccurrence('456');
+      $this->assertEquals('you',$occ2["collector"]);
+    }
+
+    public function testValidity() {
     }
 }

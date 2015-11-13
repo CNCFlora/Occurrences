77d989120db5877b966a2b1e2ad7b45aa7df50f9
Diogo Souza <diogo@diogok.net>
2015-11-13 11:23:09 -0200

    better table

--- html/js/table.js
+++ html/js/table.js
@@ -2,45 +2,91 @@
 function table() {
 
   var headers =[
-    {name:"occurrenceID",type:"string",editor:'DisabledEditor'},
-    {name:"recordedBy",type:"string",editor:'DisabledEditor'},
-    {name:"recordNumber",type:"string",editor:'DisabledEditor'},
-    {name:"collectionCode",type:"string",editor:'DisabledEditor'},
-    {name:"catalogNumber",type:"string",editor:'DisabledEditor'},
-    {name:"year",type:"string",editor:'DisabledEditor'},
-    {name:"stateProvince",type:"string",editor:'DisabledEditor'},
-    {name:"municipality",type:"string",editor: 'DisabledEditor'},
-    {name:"locality",type:"string",editor: 'DisabledEditor'},
-    {name:"remarks",type:"string",editor:'DisabledEditor'},
-    {name:"comments",type:"string",editor:'DisabledEditor'},
-    {name:"decimalLatitude",type:"float"},
-    {name:"decimalLongitude",type:"float",},
-    {name:"georeferenceVerificationStatus",type:"string",editor:'SelectEditor',editorProps:{values: ['','ok','nok','uncertain-locality'] }},
-    {name:"georeferenceProtocol",type:"string",editor:'SelectEditor',editorProps:{values: ['','coletor','sig'] }},
-    {name:"coordinateUncertaintyInMeters",type:"string"}
+    {name:"occurrenceID",editor:false, width: 600},
+    {name:"recordedBy",editor:false, width: 150},
+    {name:"recordNumber",editor:false, width: 150},
+    {name:"collectionCode",editor:false, width: 350},
+    {name:"catalogNumber",editor:false, width: 150},
+    {name:"year",editor:false, width: 50},
+    {name:"stateProvince",editor:false, width: 300},
+    {name:"municipality",editor: false, width: 300},
+    {name:"locality",editor: false, width: 350},
+    {name:"remarks",editor:false, width: 400},
+    {name:"comments",editor:false, width: 400},
+    {name:"decimalLatitude",editor:"text", width: 200},
+    {name:"decimalLongitude",editor:"text", width: 200},
+    {name:"georeferenceVerificationStatus",editor:'select',selectOptions: ['','ok','nok','uncertain-locality']},
+    {name:"georeferenceProtocol",editor:'select',selectOptions: ['','coletor','sig','google earth']},
+    {name:"coordinateUncertaintyInMeters",editor:'text'}
   ];
 
-  var rows=occurrences;
+  var fields= headers.map(function(h){return h.name});
 
-  var rows=occurrences;
+  var data = occurrences.map(function(occ){
+    var row =[];
+    for(var f=0;f<fields.length;f++){
+      row.push(occ[fields[f]]);
+    }
+    return row;
+  });
 
-  var opts={
-    sortable: true
-  };
+  var container=document.getElementById('table');
 
-  var grid= new Supagrid({
-    fields: headers.map(function(h){return h.name}),
-    data: occurrences,
-    element: document.getElementById('table'),
-    on: {
-      change: function(obj,field,value){
-        $("#saving").show();
-        $.post(base+'/'+db+'/occurrence/'+encodeURIComponent(obj.occurrenceID)+'/data/'+field,'value='+encodeURIComponent(value),function(a,b){
-          $("#saving").hide();
-        });
+  var renderer = function(instance, td, row, col, prop, value, cellProperties) {
+      Handsontable.renderers.TextRenderer.apply(this, arguments);
+      var geo = occurrences[row]['georeferenceVerificationStatus'];
+      if(typeof geo != "string") {
+        td.style.color = 'blue';
+      } else if(geo == 'ok') {
+        td.style.color = 'green';
+      } else if(geo == 'nok') {
+        td.style.color = 'gray';
+      } else {
+        td.style.color = 'blue';
       }
+  }
+
+  var hot = new Handsontable(container,{
+    data:data,
+    colHeaders: fields,
+    columns: headers,
+    rowHeaders:true,
+    columnSorting: true,
+    undo: true,
+    allowInsertColumn:false,
+    allowInsertRow:false,
+    allowRemoveColumn:false,
+    allowRemoveRow:false,
+    manualColumnResize: true,
+    cells: function(row,col,prop){
+      return { renderer : renderer };
     }
   });
 
+  var changed = function(changes,source) {
+    if(source != 'edit') return;
+    $("#saving").show();
+    var todo=changes.length;
+    var done=0;
+    for(var i=0;i<todo;i++) {
+      var row=changes[i][0];
+      var col=changes[i][1];
+      var old=changes[i][2];
+      var val=changes[i][3];
+      var obj=occurrences[row];
+      var fld=fields[col];
+      var id =obj.occurrenceID;
+      var url=base+'/'+db+'/occurrence/'+encodeURIComponent(id)+'/data/'+fld;
+      var dat='value='+encodeURIComponent(val);
+      $.post(url,dat,function(a,b){
+        done++;
+        if(done == todo) {
+          $("#saving").hide();
+        }
+      });
+    }
+  };
+
+  Handsontable.hooks.add("afterChange",changed,hot);
 }
 
